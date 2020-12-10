89cee7c4105aa9c0d5dafe17dbc4bb1994780d29
Ricky Jiang <ricky@skilledd.com>
2020-12-10 10:51:34 +0800

    GitHub action setup

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -15,7 +15,7 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Primary Button',
 };
 
 export const Secondary = Template.bind({});
